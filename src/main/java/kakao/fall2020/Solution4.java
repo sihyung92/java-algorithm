cd726c2f45e889dc45c0365830a7f7af7e2c10d5
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-10-03 20:06:24 +0900

    feat : 카카오 2020 블라인드 4번 테스트케이스 작성

---
+++ src/main/java/kakao/fall2020/Solution4.java
@@ -0,0 +1,8 @@
+package kakao.fall2020;
+
+public class Solution4 {
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+
+        return 0;
+    }
+}
--- src/test/java/kakao/fall2020/Solution1Test.java
+++ src/test/java/kakao/fall2020/Solution1Test.java
@@ -4,8 +4,11 @@ import kakao.internship.Solution1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,5 +39,21 @@ class Solution1Test {
         int[] expected = {1,1,1,1,2,4};
         kakao.fall2020.Solution3 solution1 = new kakao.fall2020.Solution3();
         assertThat(solution1.solution(info, query)).isEqualTo(expected);
+    }
+
+    @DisplayName("4번 문제")
+    @ParameterizedTest
+    @MethodSource
+    void solution4(int n, int s, int a, int b, int[][] fares, int result) {
+        kakao.fall2020.Solution4 solution4 = new kakao.fall2020.Solution4();
+        assertThat(solution4.solution(n,s,a,b,fares)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> solution4() {
+        return Stream.of(
+                Arguments.of(6,5,6,2,new int[][]{{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}},82),
+                Arguments.of(7,3,4,1,new int[][]{{5, 7, 9}, {4, 6, 4}, {3, 6, 1}, {3, 2, 3}, {2, 1, 6}},14),
+                Arguments.of(6,4,5,6,new int[][]{{2,6,6}, {6,3,7}, {4,6,7}, {6,5,11}, {2,5,12}, {5,3,20}, {2,4,8}, {4,3,9}},18)
+        );
     }
 }
